12ef4439b3cc729e3827f25dcfe48ec2db1cef34
Luke Weiler <lukew25073@gmail.com>
2021-08-06 17:18:51 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ttbot',
-    version='0.0.1',
+    version='0.0.2',
     description='Framework to build bots for TikTok',
     url='https://github.com/lukew3/ttbot',
     author='Luke Weiler',
